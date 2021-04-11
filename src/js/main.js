20b8b68d861468baee4e491097e78e72d7d834ae
Eugene <clackd1amond@users.noreply.github.com>
2021-04-11 12:41:06 +0300

    closest and siblings

--- src/js/main.js
+++ src/js/main.js
@@ -4,13 +4,4 @@ $('button').click(function () {
 	$('div').eq(2).toggleClass('active');
 });
 
-console.log($('div').attr('name'));
-
-console.log($('button').html());
-console.log($('button').html('Press me'));
-
-$('div').click(function () {
-	console.log($(this).index());
-});
-
-console.log($('div').eq(2).find('.some'));
+console.log($('.findme').siblings());
