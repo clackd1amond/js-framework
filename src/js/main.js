4fb654b0dc98b370f3e38e1108d45b68cdcde8e2
Eugene <clackd1amond@users.noreply.github.com>
2021-04-11 12:06:54 +0300

    eq, index, find

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,16 @@
 import './lib/lib';
 
 $('button').click(function () {
-	$(this).toggleClass('active');
+	$('div').eq(2).toggleClass('active');
 });
 
 console.log($('div').attr('name'));
+
+console.log($('button').html());
+console.log($('button').html('Press me'));
+
+$('div').click(function () {
+	console.log($(this).index());
+});
+
+console.log($('div').eq(2).find('.some'));
